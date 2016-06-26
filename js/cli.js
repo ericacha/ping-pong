c699fde7feb86d14e0150abe4a86dcd530e8fc91
erica <chae_osu@yahoo.com>
2016-06-26 00:14:20 -0700

    cli runs successfully.

--- js/cli.js
+++ js/cli.js
@@ -1,10 +1,14 @@
 //cli stands for command line interface
 
 var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
 
-var goal = prompt('What number would you like me to ping-pong up to?');
-// console.log('Ping-ponging up to 100!');
-var result = pingPong(goal);
-result.forEach(function(element) {
-  console.log(element);
-});
+prompt.get('goal', function(err,result) {
+  // var goal = prompt('What number would you like me to ping-pong up to?');
+  // console.log('Ping-ponging up to 100!');
+  var result = pingPong(result.goal);
+  result.forEach(function(element) {
+    console.log(element);
+  });
+})
